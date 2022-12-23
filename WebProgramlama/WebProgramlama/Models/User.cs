71e14304fd4cfebbe8df30c06caad618f9ca6e42
BurakOrtakuz <burakortakuz@gmail.com>
2022-12-23 23:19:57 +0300

    Model oluşturuldu

---
+++ WebProgramlama/WebProgramlama/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebProgramlama.Models
+{
+    public class User
+    {
+        [Required]
+        public string username { get; set; }
+
+        [Required]
+        [PasswordPropertyText]
+        public string password { get; set; }
+    }
+}
