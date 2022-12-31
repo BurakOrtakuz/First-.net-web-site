461a5512ee8ad61e04f5e61aa6f8db03220cd4e9
BurakOrtakuz <burakortakuz@gmail.com>
2022-12-31 23:14:06 +0300

    Controller işlemleri

--- WebProgramlama/WebProgramlama/Controllers/RandevuController.cs
+++ WebProgramlama/WebProgramlama/Controllers/RandevuController.cs
@@ -1,26 +1,12 @@
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using WebProgramlama.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace WebProgramlama.Controllers
 {
     public class RandevuController : Controller
     {
-        private readonly ILogger<RandevuController> _logger;
-
-        public RandevuController(ILogger<RandevuController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Randevual()
+        public IActionResult Index()
         {
             return View();
-        }
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
